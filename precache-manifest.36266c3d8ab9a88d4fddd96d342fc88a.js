b3a5a7ec1caee0d48a46e2fb29285d8672897dac
Robert Lee <55764020+robertlee12379@users.noreply.github.com>
2020-07-13 16:08:10 -0500

    Updates

---
+++ precache-manifest.36266c3d8ab9a88d4fddd96d342fc88a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14df8458c8df96084afe4081d61fe250",
+    "url": "/to_do_react/index.html"
+  },
+  {
+    "revision": "0ce47fd6f2aab79dba41",
+    "url": "/to_do_react/static/js/2.122ca384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to_do_react/static/js/2.122ca384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5440cd54b70b1c5ef13",
+    "url": "/to_do_react/static/js/main.3e8e986b.chunk.js"
+  },
+  {
+    "revision": "b02b123b0672b16e1cfc",
+    "url": "/to_do_react/static/js/runtime-main.2f7c1cdf.js"
+  }
+]);
